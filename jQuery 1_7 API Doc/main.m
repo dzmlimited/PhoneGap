f59440b470c0cbe7ca8f1648c50bac039787c2fb
Dave Morris <dzmlimited@googlemail.com>
2012-02-20 22:22:09 +0000

    jQuery 1.7 Documentation API

---
+++ jQuery 1_7 API Doc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  jQuery 1_7 API Doc
+//
+//  Created by Dave Morris on 20/02/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
